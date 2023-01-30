8b360a7087b7980ccd56845d9715f58850e483c1
Georgia Savva <P2565675@my365.dmu.ac.uk>
2023-01-30 14:11:33 +0000

    changes

--- scripts.js
+++ scripts.js
@@ -3,3 +3,14 @@ function greetMe(){
 }
 
 myTrigger.addEventListener('click',greetMe);
+
+
+let clickCount = 0;
+
+function incrementClickCount() {
+  clickCount++;
+	target.textContent = clickCount;
+}
+btnThis.addEventListener('click', incrementClickCount);
+
+myTrigger.addEventListener('click', incrementClickCount);
